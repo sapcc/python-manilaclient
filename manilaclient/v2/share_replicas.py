c8bbf2e2fe8e1f79e0d612f46c029b01e65364c6
Chuan <chuan137@gmail.com>
2024-05-28 10:12:31 +0200

    add --force flag to replica promote command (#4)
    backend implementation https://github.com/sapcc/manila/pull/175
    
    (cherry picked from commit 2fbccf1824f68d1ed648ec0ffae0abc15aaf661d)

--- manilaclient/v2/share_replicas.py
+++ manilaclient/v2/share_replicas.py
@@ -94,23 +94,23 @@ class ShareReplicaManager(base.ManagerWithFind):
 
     @api_versions.wraps("2.11", constants.REPLICA_PRE_GRADUATION_VERSION)
     @api_versions.experimental_api
-    def promote(self, replica):
+    def promote(self, replica, force):
         """Promote the provided replica.
 
         :param replica: either replica object or its UUID.
         """
-        return self._action('promote', replica)
+        return self._action('promote', replica, {'force': force})
 
     @api_versions.wraps(constants.REPLICA_GRADUATION_VERSION, '2.74')  # noqa
-    def promote(self, replica):  # noqa F811
+    def promote(self, replica, force):  # noqa F811
         """Promote the provided replica.
 
         :param replica: either replica object or its UUID.
         """
-        return self._action('promote', replica)
+        return self._action('promote', replica, {'force': force})
 
     @api_versions.wraps('2.75')  # noqa
-    def promote(self, replica, quiesce_wait_time=None):  # noqa F811
+    def promote(self, replica, force, quiesce_wait_time=None):  # noqa F811
         """Promote the provided replica.
 
         :param replica: either replica object or its UUID.
@@ -119,6 +119,8 @@ class ShareReplicaManager(base.ManagerWithFind):
         body = None
         if quiesce_wait_time:
             body = dict(quiesce_wait_time=quiesce_wait_time)
+
+        body.update({'force': force})
         return self._action('promote', replica, body)
 
     @api_versions.wraps("2.11", constants.REPLICA_PRE_GRADUATION_VERSION)
